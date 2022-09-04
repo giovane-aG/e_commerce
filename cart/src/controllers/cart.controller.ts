971108ec9124016b88c39fbe1ce45e0d6af81d7d
Giovane <giovaneaalmeida27@gmail.com>
2022-09-04 20:11:56 -0300

    feat: createUserCart

--- cart/src/controllers/cart.controller.ts
+++ cart/src/controllers/cart.controller.ts
@@ -5,11 +5,14 @@ export class CartController {
   private readonly cartService: CartService
 
   constructor () {
-    this.cartService = this.cartService
+    this.cartService = new CartService()
   }
 
-  
   async getUserCart(id: string) {
     return this.cartService.getUserCart(id)
+  }
+
+  async createUserCart(userId: string) {
+    return this.cartService.createUserCart(userId)
   }
 }
