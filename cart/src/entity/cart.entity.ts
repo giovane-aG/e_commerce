88775c825ce50f63aea5b90aebf560cdab4645bc
Giovane <giovaneaalmeida27@gmail.com>
2022-09-11 20:54:47 -0300

    refact: remove relation

--- cart/src/entity/cart.entity.ts
+++ cart/src/entity/cart.entity.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata"
 import { Entity, PrimaryColumn, Column, OneToOne, ManyToOne, OneToMany, JoinColumn } from 'typeorm'
-import { CartProduct } from "./cart-product.entity"
-import { User } from './user.entity'
 
 @Entity()
 export class Cart {
@@ -9,7 +7,5 @@ export class Cart {
   id: string
 
   @Column()
-  @OneToOne(() =>  User)
-  @JoinColumn({ name: 'id', referencedColumnName: 'id', foreignKeyConstraintName: 'fk_user_id' })
   userId: string
 }
