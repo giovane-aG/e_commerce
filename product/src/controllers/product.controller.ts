9e7accc005934b35c7c64dd1265cc498e337ba04
Giovane <giovaneaalmeida27@gmail.com>
2022-09-18 17:46:17 -0300

    feat: start product controller

---
+++ product/src/controllers/product.controller.ts
@@ -0,0 +1,21 @@
+import { CreateProductDTO } from "../dtos/create-product.dto";
+import { PatchProductDTO } from "../dtos/patch-product.dto";
+import { Product } from "../entities/product.entity";
+import { ProductService } from "../services/product.service";
+
+
+export class ProductController {
+  private readonly productService: ProductService
+
+  constructor () {
+    this.productService = new ProductService()
+  }
+
+  async createProduct(createProductDTO: CreateProductDTO): Promise<Product> {
+    return this.productService.createProduct(createProductDTO)
+  }
+
+  async patchProduct(id: string, patchProductDTO: PatchProductDTO): Promise<void> {
+    this.productService.patchProduct(id, patchProductDTO)
+  }
+}
