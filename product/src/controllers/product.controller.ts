116f4459ce767e67adba56df6cbb088e1d2b9da9
Giovane <giovaneaalmeida27@gmail.com>
2022-09-20 20:20:01 -0300

    feat: create getProduct handler

--- product/src/controllers/product.controller.ts
+++ product/src/controllers/product.controller.ts
@@ -3,12 +3,15 @@ import { PatchProductDTO } from "../dtos/patch-product.dto";
 import { Product } from "../entities/product.entity";
 import { ProductService } from "../services/product.service";
 
-
 export class ProductController {
   private readonly productService: ProductService
 
   constructor () {
     this.productService = new ProductService()
+  }
+
+  async getProducts(filters: any = null): Promise<Product[]> {
+    return this.productService.getProducts(filters)
   }
 
   async createProduct(createProductDTO: CreateProductDTO): Promise<Product> {
