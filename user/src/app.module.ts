865c9f8580c7a83211c41e518f44e7a5f690a9e0
Giovane <giovaneaalmeida27@gmail.com>
2022-09-28 21:13:49 -0300

    config: database connection

--- user/src/app.module.ts
+++ user/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user/user.entity'
@@ -11,14 +9,14 @@ import { User } from './user/user.entity'
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: 8000,
       username: 'postgres',
       password: 'postgres',
-      database: 'e_commerce',
+      database: 'postgres',
       entities: [ User ]
     })
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
